2f5f5901554d9cea240f0b94b939fe67cbf5ea9c
Ethan Phan <ethanphanvn@gmail.com>
2020-10-08 00:03:38 -0400

    new change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send({ 2: "there"})
-})
+  res.send({ buy: "buddy" });
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
