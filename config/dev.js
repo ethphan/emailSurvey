2188d494c14c806dec1e74c207120f59b0768700
Ethan Phan <ethanphanvn@gmail.com>
2020-10-22 18:28:04 -0400

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: "1006957113077-ahq51945q8nru8ou79n6v3j1cd1q46e6.apps.googleusercontent.com",
+    googleClientSecret: "8ooT0ftAgjOf-cqtok4Gu4Kp",
+    mongoURI: 'mongodb+srv://ethan:eHHbTYNvc06KiuwH@emaily.vzbzt.mongodb.net/Emaily?retryWrites=true&w=majority',
+    cookieKey: "redacaadsfasdfasdfasdfasdfasdfasdfasdfadsfasdfasdfted"
+}
+
+//mongodb+srv://ethan:091292@cluster0.ev5d0.mongodb.net/ethan?retryWrites=true&w=majority
