6a7ea9443aedce0c4cb95390b71eca2e240f117f
Ethan Phan <ethanphanvn@gmail.com>
2020-11-04 21:05:35 -0500

    added billing and client side app

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,9 @@ module.exports = {
     googleClientID: "1006957113077-ahq51945q8nru8ou79n6v3j1cd1q46e6.apps.googleusercontent.com",
     googleClientSecret: "8ooT0ftAgjOf-cqtok4Gu4Kp",
     mongoURI: 'mongodb+srv://ethan:eHHbTYNvc06KiuwH@emaily.vzbzt.mongodb.net/Emaily?retryWrites=true&w=majority',
-    cookieKey: "redacaadsfasdfasdfasdfasdfasdfasdfasdfadsfasdfasdfted"
+    cookieKey: "redacaadsfasdfasdfasdfasdfasdfasdfasdfadsfasdfasdfted",
+    stripePublishableKey: 'pk_test_51HiZ8aIKDmPRq8sUmErYle5bAIIafCGPM1fAjh8rDhbxXPA5bA8I1Iy4boNPAuaF6RxkHRtgbHUqsaXewsB4WlbD00pAYKJJHc',
+    stripeSecretKey: 'sk_test_51HiZ8aIKDmPRq8sUB5iJglh3EkwtoMLpusoCzMCuQTJYiw3q0zHhBP6id5E6kNkM56nNj4PkKsD3otLMp98zrI1o00W35Z6vqZ'
 }
 
 //mongodb+srv://ethan:091292@cluster0.ev5d0.mongodb.net/ethan?retryWrites=true&w=majority
